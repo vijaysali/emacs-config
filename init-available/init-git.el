c31d7d330915793013a92c3edc3e068dbb903443
Isaac <icylisper@gmail.com>
2014-02-28 17:13:43 +0530

    Add magit as dependency

--- init-available/init-git.el
+++ init-available/init-git.el
@@ -1,3 +1,6 @@
+(require 'packages)
+(install-packages '(magit))
+
 (eval-after-load 'diff-mode
   '(progn
      (set-face-foreground 'diff-added "green4")
@@ -7,3 +10,5 @@
   '(progn
      (set-face-foreground 'magit-diff-add "green3")
      (set-face-foreground 'magit-diff-del "red3")))
+
+(provide 'init-git)
