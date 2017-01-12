c498d2c9aacf3efa69fedffa871e49693fa9b334
Abhijith <abhijithg@gmail.com>
2017-01-12 23:15:01 +0530

    version the backup files

--- init.el
+++ init.el
@@ -1,10 +1,10 @@
 (progn
-  (set-face-attribute 'default nil :height 85)
   (add-to-list 'load-path (expand-file-name "~/.emacs.d/init-enabled") t)
   (add-to-list 'load-path (expand-file-name "~/.emacs.d/themes") t)
   (add-to-list 'load-path (expand-file-name "~/.emacs.d/lisp") t)
   (add-to-list 'custom-theme-load-path "~/.emacs.d/themes")
   (add-to-list 'default-frame-alist '(fullscreen . maximized))
+
   (setq
    frame-title-format '(buffer-file-name "%f" ("%b"))
    indent-tabs-mode nil
@@ -28,10 +28,18 @@
   (mouse-wheel-mode t)
   (blink-cursor-mode t)
   (recentf-mode 0)
+
+  (setq backup-directory-alist `(("." . "~/.emacs.d/.saves")))
+  (setq delete-old-versions t
+	kept-new-versions 6
+	kept-old-versions 2
+	version-control t)
+
   (mapc (lambda (name)
-        (require (intern (file-name-sans-extension name))))
+	  (require (intern (file-name-sans-extension name))))
 	(directory-files "~/.emacs.d/init-enabled" nil "\\.el$"))
   (setq custom-file "~/.emacs.d/emacs-custom.el")
   (when (file-exists-p custom-file)
     (load custom-file))
-  (load-theme 'dusk))
+  (load-theme 'dusk)
+  (set-face-attribute 'default nil :height 85))
